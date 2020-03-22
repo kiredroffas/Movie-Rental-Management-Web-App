52f6f6a952f30cd40dffce979ffa1c3c821d3b1d
Erik <erik.safford@wsu.edu>
2020-03-21 20:10:19 -0700

    Added Movie controller and model at Movies/Random, added new Bootstrap theme

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movie_Rental_Management_Web_App.Models;
+
+namespace Movie_Rental_Management_Web_App.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
